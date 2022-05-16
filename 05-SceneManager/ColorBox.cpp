720fbdf00f3c186ac4dec5c7eb049c81d39260f3
DESKTOP-RVTKMQU\Miin <ktran97>
2022-05-16 20:52:21 +0700

    Add pipe -> Render pipe after plant, Update colorbox

--- 05-SceneManager/ColorBox.cpp
+++ 05-SceneManager/ColorBox.cpp
@@ -1,4 +1,18 @@
 #include "ColorBox.h"
+ColorBox::ColorBox(int W, int H)
+{
+	this->Width = W;
+	this->Height = H;
+	this->objType = 6;
+}
+
+void ColorBox::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	left = x - Width / 2;
+	top = y - Height / 2;
+	right = left + Width;
+	bottom = top + Height;
+}
 
 void ColorBox::Render()
 {
--- 05-SceneManager/ColorBox.h
+++ 05-SceneManager/ColorBox.h
@@ -6,19 +6,9 @@ class ColorBox :
 	int Width;
 	int Height;
 public:
-	ColorBox(int W, int H)
-	{
-		Width = W;
-		Height = H;
-		objType = 6;
-	}
-	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom)
-	{
-		left = x - Width / 2;
-		top = y - Height / 2;
-		right = left + Width;
-		bottom = top + Height;
-	}
+	ColorBox(int W, int H);
+
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual int GetWidth() { return Width; };
 	virtual void Render();
 	virtual void DirectBlocking(int& l, int& t, int& r, int& b) { l = 0; t = 1; b = 0; r = 0; }
